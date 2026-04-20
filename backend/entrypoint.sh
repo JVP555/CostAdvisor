97ae05083c1ab8cfaba1f011763981db5514f2ac
Alexis <alexisthomasgm@gmail.com>
2026-04-20 12:23:32 +0200

    Fix HTTPS redirects behind proxy

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,4 +2,4 @@
 /usr/sbin/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 sleep 2
 tailscale up --auth-key=$TAILSCALE_AUTHKEY --hostname=railway-api-$(hostname)
-exec uvicorn app.main:app --host 0.0.0.0 --port $PORT
+exec uvicorn app.main:app --host 0.0.0.0 --port $PORT --proxy-headers --forwarded-allow-ips='*'
